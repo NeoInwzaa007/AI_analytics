31d7065e86a6ec9b8e92ea4111d41cbcc8d4d1e0
Chattanong Dilloksirichart <chattanong.d@gmail.com>
2026-03-18 17:27:11 +0700

    force api debug

--- backend_service/main.py
+++ backend_service/main.py
@@ -64,7 +64,7 @@ from src.routers import dashboard
 app.include_router(dashboard.router)
 app.include_router(connections.router)
 
-print("AUTH ROUTER LOADED")
+print("AUTH ROUTER LOADED SUCCESSFULLY")
 from src.routers.auth import router as auth_router
 app.include_router(auth_router)
 
